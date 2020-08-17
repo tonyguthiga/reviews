9a9f675e07f4d67247b91daf68ab219a584cdb96
antony-guthiga <toniguthiga93@gmail.com>
2020-08-17 23:54:50 +0300

    add url paths

---
+++ project/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^update/profile$', views.updateprofile, name='update'),
+    url(r'^profile/$', views.profile, name='profile'),
+    url(r'^new/post$', views.new_post, name='new_post'), 
+    url(r'^search/', views.search_results, name='search_results'),
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
