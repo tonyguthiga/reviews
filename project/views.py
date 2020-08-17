636877d2760edf6a1be9136b4b2629dac9b1e264
antony-guthiga <toniguthiga93@gmail.com>
2020-08-17 23:56:18 +0300

    add index view function, add profile view function, add update_profile view function, add new_post view function, add search view function.

---
+++ project/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from .models import Profile, Post
+import datetime as dt
+from . forms import ProfileForm, PostForm
+
+# Create your views here.
+def index(request):
+    date =dt.date.today()
+    posts = Post.objects.all()
+    return render(request, 'index.html', {"date":date, "posts":posts })
+
+@login_required(login_url='/accounts/login/?next=/')
+def profile(request):
+    current_user = request.user
+    profile = Profile.objects.filter(user=current_user).first()
+    posts = request.user.post_set.all()
+
+    return render(request, 'profile.html', locals())
+
+@login_required(login_url='/accounts/login/?next=/')
+def updateprofile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            add = form.save(commit=False)
+            add.user = current_user
+            add.save()
+            return redirect('profile')
+    else:
+        form = ProfileForm()
+    return render(request, 'profile_update.html',{"form":form })
+
+@login_required(login_url='/accounts/login/?next=/')
+def new_post(request):
+        current_user = request.user
+        if request.method == 'POST':
+                form = PostForm(request.POST, request.FILES)
+                if form.is_valid():
+                        add=form.save(commit=False)
+                        add.user = current_user
+                        add.save()
+                return redirect('index')
+        else:
+                form = PostForm()
+                return render(request,'new_post.html', {"form":form})
+
+
+def search_results(request):
+
+    if 'post' in request.GET and request.GET["post"]:
+        search_term = request.GET.get("post")
+        searched_posts = Post.search(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"posts": searched_posts})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
