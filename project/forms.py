a2f58f8e4c390ddef69a13584cd84c76a58d19d8
antony-guthiga <toniguthiga93@gmail.com>
2020-08-17 23:53:00 +0300

    add profile form and postform

---
+++ project/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Profile, Post
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude=["user"]
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = []
