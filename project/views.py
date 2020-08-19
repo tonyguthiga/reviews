d47b69bc2d5dabc4c79ed178ad06ca50862cb0d3
antony-guthiga <toniguthiga93@gmail.com>
2020-08-19 09:14:33 +0300

    add rating view function

--- project/views.py
+++ project/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponse, Http404, HttpResponseRedirect
-from .models import Profile, Post
+from .models import Profile, Post, Rating
 import datetime as dt
-from . forms import ProfileForm, PostForm
+from . forms import ProfileForm, PostForm, RatingsForm
 
 # Create your views here.
 def index(request):
@@ -61,3 +61,61 @@ def search_results(request):
     else:
         message = "You haven't searched for any term"
         return render(request, 'search.html',{"message":message})
+
+def site(request,site_id):
+    current_user = request.user
+    profile = Profile.objects.filter(user=current_user).first()
+
+    try:
+        post= Post.objects.get(id=site_id)
+    except:
+        raise ObjectDoesNotExist()
+
+    try:
+        ratings = Rating.objects.filter(post_id=site_id)
+        design = Rating.objects.filter(post_id=site_id).values_list('design',flat=True)
+        usability = Rating.objects.filter(post_id=site_id).values_list('usability',flat=True)
+        creativity = Rating.objects.filter(post_id=site_id).values_list('creativity',flat=True)
+        content = Rating.objects.filter(post_id=site_id).values_list('content',flat=True)
+        total_design=0
+        total_usability=0
+        total_creativity=0
+        total_content = 0
+
+        for rate in design:
+            total_design+=rate
+
+        for rate in usability:
+            total_usability+=rate
+
+        for rate in creativity:
+            total_creativity+=rate
+
+        for rate in content:
+            total_content+=rate
+
+        overall_score=(total_design+total_content+total_usability+total_creativity)/4
+
+        post.design = total_design
+        post.usability = total_usability
+        post.creativity = total_creativity
+        post.content = total_content
+        post.overall_score = overall_score
+
+        post.save()
+
+    except:
+        return None
+
+    if request.method =='POST':
+        form = RatingsForm(request.POST,request.FILES)
+        if form.is_valid():
+            rating = form.save(commit=False)
+            rating.post= post
+            rating.profile = profile
+            rating.overall_score = (rating.design+rating.usability+rating.creativity+rating.content)/2
+            rating.save()
+    else:
+        form = RatingsForm()
+
+    return render(request,"site.html",{"post":post,"profile":profile,"ratings":ratings,"form":form})
